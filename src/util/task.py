58275947a77a77413aefd1087b2539c899db9eeb
Pasquale Capuano <p.capuano@informatica.aci.it>
2021-06-13 03:35:22 +0200

    Migliorie nella definizione ed esecuzione task

--- src/util/task.py
+++ src/util/task.py
@@ -6,31 +6,50 @@ Created on Wed Jul 11 18:53:22 2018
 """
 
 import cmd
+import concurrent.futures as cf
 import schedule
 import subprocess
 import threading
 import time
 
+from collections import defaultdict, ChainMap
 from telegram.ext import Updater, CommandHandler
 from util.common import Wrap
 from util.security import security_decode
 
-TASK_SUBPROCESS = "subprocess"
+TASK_TYPE_SUBPROCESS = "subprocess"
 
-class TaskDef(object):
+TASK_RUN_SINGLETON = -1
+TASK_RUN_CONFLICT = -2
+
+class Task(object):
         
-    def __init__(self, name, type, logger, description=None, conflict=None, args=None):
+    def __init__(self, name, type, logger, options):
         self._name = name
         self._type = type
-        self._description = description
-        self._conflict = conflict
-        self._args = args
         self._logger = logger
-        self._command = None
+        
+        self._description = options.get("description")
+        self._singleton = options.get("singleton")
+        self._conflict = options.get("conflict")
+        self._args = options.get("args")
+        self._command = options.get("command")
+        
         self._schedule = dict()
         self._runnable = True
-        self._singleton = True
+        
+        self._init_default_args()
     
+    
+    def _init_default_args(self):
+        self._default_args = dict()
+        
+        try:
+            for arg in self._args:
+                if arg.default is not None:
+                    self._default_args[arg.name] = eval(f"{arg.type}({arg.default})")
+        except:
+            self._logger.warning(f"Cannot set default args for task '{self._name}': missing definition?")
     
     @property
     def name(self):
@@ -41,20 +60,24 @@ class TaskDef(object):
         return self._type
     
     @property
+    def description(self):
+        return self._description
+    
+    @property
+    def conflict(self):
+        return self._conflict
+    
+    @property
+    def args(self):
+        return self._args
+    
+    @property
     def command(self):
         return self._command
-    
-    @command.setter
-    def command(self, value):
-        self._command = value
     
     @property
     def singleton(self):
         return self._singleton
-    
-    @singleton.setter
-    def singleton(self, value):
-        self._singleton = bool(value)
     
     @property
     def runnable(self):
@@ -84,16 +107,16 @@ class TaskDef(object):
             task_schedule.do(self)
     
     def __call__(self, *args, **kwargs):
-        self._logger.info("Running task \"{0}\"".format(self.name))
+        self._logger.info(f"Running task '{self.name}'")
         
-        if self.type == TASK_SUBPROCESS:
-            self._logger.debug("Invoking command: {0}".format(self.command))
+        if self.type == TASK_TYPE_SUBPROCESS:
+            self._logger.debug(f"Invoking command: {self.command}")
             
-            self.command.bind_context(kwargs)
+            self.command.bind_context(ChainMap(kwargs, self._default_args))
             
             subprocess.call(self.command)
 
-            self._logger.info("Task \"{0} invoked".format(self.name))
+            self._logger.info(f"Task '{self.name}' invoked")
 
 class TaskShell(cmd.Cmd):
     intro = "TaskManager runtime shell. Type 'help' or '?' for available commands.\n"
@@ -111,13 +134,15 @@ class TaskShell(cmd.Cmd):
     def do_run(self, arg):
         "Run a task by name."
         
-        name = arg.strip()
+        args = [s.strip() for s in arg.split(" ")]
         
+        name = args[0]
+        kwargs = Wrap.prepare_context(args[1:])
         task = self._manager.get_task(name)
         
         if task is not None:
             self._logger.debug(f"Running command {name}")
-            task()
+            result = self._manager.run_task(task, kwargs)
         else:
             self._logger.error(f"No task found with name '{name}'")
         
@@ -128,6 +153,13 @@ class TaskShell(cmd.Cmd):
         
         for task_name in sorted(self._manager.task_list.keys()):
             print(f"\t{task_name}")
+        
+        return False
+    
+    def do_task_status(self, arg):
+        "Print task execution status."
+        
+        self._manager.task_status()
         
         return False
     
@@ -176,11 +208,17 @@ class TaskShell(cmd.Cmd):
     def do_shutdown(self, arg):
         "Stop the shell and all the running services."
         
+        print("Stopping services...")
+        
         if self._manager.has_scheduler:
             self._manager.scheduler.stop()
+            print("Scheduler stopped")
         
         if self._manager.has_telegram:
             self._manager.telegram.stop()
+            print("Telegram stopped")
+        
+        print("TaskManager will stop after all running tasks complete.")
         
         return True
     
@@ -241,7 +279,6 @@ class TaskTelegramController(object):
         self._updater = Updater(token, use_context=True)
         self._dispatcher = self._updater.dispatcher
         
-        self._dispatcher.add_handler(CommandHandler("test", self.do_test))
         self._dispatcher.add_handler(CommandHandler("run", self.do_run))
         self._dispatcher.add_handler(CommandHandler("tasklist", self.do_tasklist))
         
@@ -250,10 +287,6 @@ class TaskTelegramController(object):
     @property
     def running(self):
         return self._running
-    
-    def do_test(self, update, context):
-        print(">>> update <<<\n", update)
-        print(">>> context <<<\n", context.args)
     
     def do_run(self, update, context):
         "Run a task by name."
@@ -268,7 +301,16 @@ class TaskTelegramController(object):
                 
                 self._logger.debug(message)
                 kwargs = Wrap.prepare_context(context.args)
-                task(**kwargs)
+                
+                result = self._manager.run_task(task, kwargs)
+                
+                if isinstance(result, int):
+                    if result == TASK_RUN_SINGLETON:
+                        message = f"Task '{task.name}' is singleton and is running just now: cannot run again"
+                    elif result == TASK_RUN_CONFLICT:
+                        message = f"Task '{task.name}' cannot be run because another conflicting task is already running"
+                    else:
+                        message = f"Cannot run task '{task.name}' just now"
             else:
                 message = f"No task found with name '{name}'"
                 
@@ -310,7 +352,7 @@ class TaskTelegramController(object):
             self._logger.info("TaskTelegramController stopped.")
         else:
             self._logger.info("TaskTelegramController not running.")
-        
+
 class TaskManager(object):
 
     def __init__(self, config, context, logger):
@@ -319,25 +361,28 @@ class TaskManager(object):
         self._logger = logger
 
         self._task_list = dict()
+        
+        self._init_monitor()
         self._init_task_list(config.tasklist)
         self._init_shell(bool(config.shell))
         self._init_scheduler(bool(config.scheduler))
         self._init_telegram(config.telegram)
-
+        
         self._logger.info("TaskManager initialized")
+    
+    
+    def _init_monitor(self):
+        self._pool = cf.ThreadPoolExecutor(thread_name_prefix="TaskManager")
+        self._running_task = defaultdict(int)
+        self._futures = dict()
     
     
     def _init_task_list(self, task_list):
         for task in task_list:
-            self._logger.debug("Preparing task: {0}".format(task))
+            self._logger.debug(f"Preparing task: {task}")
 
-            task_def = TaskDef(task.name, task.type, self._logger, task.description, task.conflict, task.args)
-            
-            task.singleton = task.singleton
-            
-            if task.type == TASK_SUBPROCESS:
-                task_def.command = task.command
-            
+            task_def = Task(task.name, task.type, self._logger, task.to_dict())
+                        
             if bool(task.schedule):
                 for task_schedule in task.schedule:
                     interval = task_schedule[0]
@@ -347,19 +392,19 @@ class TaskManager(object):
                         job = getattr(schedule.every(interval), unit)
     
                         self._logger.debug(
-                            "Task \"{0}\": defined schedule for every {1} {2}".format(task.name, interval, unit))
+                            f"Task '{task.name}': defined schedule for every {interval} {unit}")
     
                         if len(task_schedule) > 2:
                             at_time = task_schedule[2]
                             job = job.at(at_time)
     
-                            self._logger.debug("Task \"{0}\": added run time at {1}".format(task.name, at_time))
+                            self._logger.debug(f"Task '{task.name}': added run time at {at_time}")
                         
                         job.do(task_def)
                         
                         task_def.add_schedule(job)
                     except:
-                        self._logger.error('ERROR: unit [{}] not valid!'.format(unit))
+                        self._logger.error(f"ERROR: unit [{unit}] not valid!")
             
             self._task_list[task.name] = task_def
     
@@ -435,6 +480,58 @@ class TaskManager(object):
     
     def get_task(self, name):
         return self._task_list.get(name)
+    
+    def refresh_status(self, timeout=1):
+        done, not_done = cf.wait(self._futures, timeout)
+        
+        for future in done:
+            name = self._futures[future]
+            
+            del self._futures[future]
+            
+            self._running_task[name] -= 1
+            
+        for name in list(self._running_task.keys()):
+            if self._running_task[name] == 0:
+                del self._running_task[name]
+    
+    def run_task(self, task, kwargs):
+        self.refresh_status(0.1)
+        
+        if task.singleton and self._running_task[task.name] > 0:
+            self._logger.warning(f"Task '{task.name}' is singleton and is running just now: cannot run again")
+        
+            return TASK_RUN_SINGLETON
+        
+        if task.conflict is not None and len(task.conflict) > 0:
+            if any([bool(self._running_task[name] > 0) for name in task.conflict]):
+                self._logger.warning(f"Task '{task.name}' cannot be run because another conflicting task is already running")
+                
+                return TASK_RUN_CONFLICT
+        
+        self._running_task[task.name] += 1
+        
+        future = self._pool.submit(task, **kwargs)
+        
+        self._futures[future] = task.name
+        
+        return future
+    
+    def task_status(self):
+        self.refresh_status()
+        
+        try:
+            l_n = max(10, *[len(n) for n in self._running_task.keys()])
+            
+            print(f"\nTask{' '*(l_n - 4)}|Running")
+            print(f"{'-' * l_n}+-------")
+            
+            for k,v in self._running_task.items():
+                print(f"{k}{' '*(l_n - len(k))}|{v:>7}")
+            
+            print()
+        except:
+            print("\nNo running task.\n")
     
     def execute(self):
         self._logger.info("TaskManager running...")
